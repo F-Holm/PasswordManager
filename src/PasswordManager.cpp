145bb6070e4236d75837207c4cf2b3f805478725
Holm <you@example.com>
2025-02-06 00:22:21 -0300

    cargué archivos de 'PasswordTerminal'

---
+++ src/PasswordManager.cpp
@@ -0,0 +1,13 @@
+﻿// PasswordManager.cpp : Defines the entry point for the application.
+//
+
+#include "PasswordManager.h"
+#include <openssl/sha.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake. -> " << time(0)%1000 << " -> " << SHA256_DIGEST_LENGTH << endl;
+	return 0;
+}
