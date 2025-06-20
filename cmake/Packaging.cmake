cd597a74d7d9e62ecd33dda3cef7c57f430a741d
F-Holm <holmfedericoa@gmail.com>
2025-06-20 19:24:07 -0300

    Agregué cosas a los CMakeLists.txt

---
+++ cmake/Packaging.cmake
@@ -0,0 +1,5 @@
+# CPack configuration
+set(CPACK_PACKAGE_VENDOR "Federico Holm")
+set(CPACK_PACKAGE_CONTACT "holmfedericoa@gmail.com")
+set(CPACK_PACKAGE_DESCRIPTION "Gestor de contraseñas")
+include(CPack)
