e5eba0bf0496a91bfdaed6267f698670e5d0d38e
Holm <you@example.com>
2025-07-02 17:33:44 -0300

    .cc & .hh

---
+++ src/datos/datos.hh
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include "cuenta.h"
+
+class Datos {
+private:
+  std::vector<Cuenta> cuentas;
+
+  bool idUnico(const std::string id) const;
+  std::string generarIdUnico() const;
+  static void setRand();
+
+public:
+  Datos(const std::string &key);
+  ~Datos();
+
+  const static std::string NOMBRE_ARCHIVO;
+
+  int GetIndex(const std::string ID) const;
+  std::vector<Cuenta> GetCuentas() const;
+  void SetCuentas(const std::vector<Cuenta> &cuentas);
+
+  void AgregarCuenta(Cuenta &cuenta);
+  void ModificarCuenta(const Cuenta &cuenta);
+  void EliminarCuenta(const std::string &id);
+
+  void CargarCuentas(const std::string &key);
+  void GuardarCuentas(const std::string &key);
+};
