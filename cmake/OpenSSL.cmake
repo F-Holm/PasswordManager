2de88d1c069df5e97ba8c218cdac0bb81cca199c
Holm <you@example.com>
2025-07-01 23:02:47 -0300

    add script OpenSSL

---
+++ cmake/OpenSSL.cmake
@@ -0,0 +1,25 @@
+include(FetchContent)
+
+if(NOT DEFINED OPENSSL_VERSION)
+    set(OPENSSL_VERSION openssl-3)
+endif()
+
+message(STATUS "Fetching OpenSSL version: ${OPENSSL_VERSION}")
+
+# Download
+FetchContent_Declare(
+  openssl
+  GIT_REPOSITORY https://github.com/openssl/openssl.git
+  GIT_TAG        ${OPENSSL_VERSION}
+)
+
+# Opciones de build para OpenSSL
+set(OPENSSL_BUILD_TESTING OFF CACHE BOOL "" FORCE)
+set(OPENSSL_BUILD_DOC     OFF CACHE BOOL "" FORCE)
+set(OPENSSL_USE_STATIC_LIBS ON CACHE BOOL "" FORCE)
+
+# Build
+FetchContent_MakeAvailable(openssl)
+
+# Exportar los targets
+set(OPENSSL_TARGETS OpenSSL::SSL OpenSSL::Crypto PARENT_SCOPE)
