82d247638c622251cf5f1f3838489d0aad359230
F-Holm <holmfedericoa@gmail.com>
2025-07-17 21:12:24 +0000

    apdate build-all.sh scritp

--- build-all.sh
+++ build-all.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 ./reset_modules.sh
-cmake --workflow --preset windows-x64-debug
-cmake --workflow --preset windows-x64-release
-cmake --workflow --preset windows-x86-debug
-cmake --workflow --preset windows-x86-release
-cmake --workflow --preset linux-x64-debug
-cmake --workflow --preset linux-x64-release
-cmake --workflow --preset linux-x86-debug
-cmake --workflow --preset linux-x86-release
+cmake --workflow --preset wf-windows-x64-release
+cmake --workflow --preset wf-windows-x64-debug
+cmake --workflow --preset wf-windows-x86-release
+cmake --workflow --preset wf-windows-x86-debug
+cmake --workflow --preset wf-windows-arm64-release
+cmake --workflow --preset wf-windows-arm64-debug
+cmake --workflow --preset wf-windows-arm-release
+cmake --workflow --preset wf-windows-arm-debug
+cmake --workflow --preset wf-linux-x64-release
+cmake --workflow --preset wf-linux-x64-debug
+cmake --workflow --preset wf-linux-x86-release
+cmake --workflow --preset wf-linux-x86-debug
+cmake --workflow --preset wf-linux-arm64-release
+cmake --workflow --preset wf-linux-arm64-debug
+cmake --workflow --preset wf-linux-arm-release
+cmake --workflow --preset wf-linux-arm-debug
