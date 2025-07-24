de925bf171832b68626cd58d3c2f82622eaa2145
F-Holm <moshishafcdodgecharger@gmail.com>
2025-07-24 23:07:26 +0000

    add: OPENSSL_ARCH_TARGET to presets

--- build-all.sh
+++ build-all.sh
@@ -3,8 +3,8 @@ cmake --workflow --preset wf-windows-x64-release
 cmake --workflow --preset wf-windows-x64-debug
 cmake --workflow --preset wf-windows-x86-release
 cmake --workflow --preset wf-windows-x86-debug
-#cmake --workflow --preset wf-windows-arm64-release
-#cmake --workflow --preset wf-windows-arm64-debug
+cmake --workflow --preset wf-windows-arm64-release
+cmake --workflow --preset wf-windows-arm64-debug
 #cmake --workflow --preset wf-windows-arm-release
 #cmake --workflow --preset wf-windows-arm-debug
 cmake --workflow --preset wf-linux-x64-release
