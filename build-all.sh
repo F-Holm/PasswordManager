83f7ff527df14181352c70182a6ba97f3910346a
F-Holm <moshishafcdodgecharger@gmail.com>
2025-07-28 16:42:30 +0000

    add: comments to build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+
+# Borrar /build y resetear openssl-src
 ./clean.sh
-cmake --workflow --preset wf-windows-x64-release
-cmake --workflow --preset wf-windows-x64-debug
-cmake --workflow --preset wf-windows-x86-release
-cmake --workflow --preset wf-windows-x86-debug
-cmake --workflow --preset wf-windows-arm64-release
-cmake --workflow --preset wf-windows-arm64-debug
+
+# Build workflows (los comentados todavía no están testeados pero ya están configurados)
+#cmake --workflow --preset wf-windows-x64-release
+#cmake --workflow --preset wf-windows-x64-debug
+#cmake --workflow --preset wf-windows-x86-release
+#cmake --workflow --preset wf-windows-x86-debug
+#cmake --workflow --preset wf-windows-arm64-release
+#cmake --workflow --preset wf-windows-arm64-debug
 #cmake --workflow --preset wf-windows-arm-release
 #cmake --workflow --preset wf-windows-arm-debug
 cmake --workflow --preset wf-linux-x64-release
 cmake --workflow --preset wf-linux-x64-debug
-cmake --workflow --preset wf-linux-x86-release
-cmake --workflow --preset wf-linux-x86-debug
-cmake --workflow --preset wf-linux-arm64-release
-cmake --workflow --preset wf-linux-arm64-debug
-cmake --workflow --preset wf-linux-arm-release
-cmake --workflow --preset wf-linux-arm-debug
+#cmake --workflow --preset wf-linux-x86-release
+#cmake --workflow --preset wf-linux-x86-debug
+#cmake --workflow --preset wf-linux-arm64-release
+#cmake --workflow --preset wf-linux-arm64-debug
+#cmake --workflow --preset wf-linux-arm-release
+#cmake --workflow --preset wf-linux-arm-debug
