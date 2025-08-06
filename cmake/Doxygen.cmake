6bec5d5d892d98871ae4c194f383f1b698ab946a
F-Holm <moshishafcdodgecharger@gmail.com>
2025-08-06 23:40:30 +0000

    cmake scripts: 2_space_tabs -> 4_space_tabs

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -1,43 +1,42 @@
 function(Doxygen input output)
-  find_package(Doxygen)
-  if (NOT DOXYGEN_FOUND)
-    add_custom_target(doxygen COMMAND false
-                              COMMENT "Doxygen not found")
-    return()
-  endif()
+    find_package(Doxygen)
+    if (NOT DOXYGEN_FOUND)
+        add_custom_target(doxygen COMMAND false COMMENT "Doxygen not found")
+        return()
+    endif()
 
-  set(DOXYGEN_GENERATE_HTML YES)
-  set(DOXYGEN_HTML_OUTPUT   ${PROJECT_BINARY_DIR}/${output})
+    set(DOXYGEN_GENERATE_HTML YES)
+    set(DOXYGEN_HTML_OUTPUT   ${PROJECT_BINARY_DIR}/${output})
 
-  UseDoxygenAwesomeCss()
-  UseDoxygenAwesomeExtensions()
+    UseDoxygenAwesomeCss()
+    UseDoxygenAwesomeExtensions()
 
-  doxygen_add_docs(doxygen
-      ${PROJECT_SOURCE_DIR}/${input}
-      COMMENT "Generate HTML documentation"
-  )
+    doxygen_add_docs(doxygen
+        ${PROJECT_SOURCE_DIR}/${input}
+        COMMENT "Generate HTML documentation"
+    )
 endfunction()
 
 macro(UseDoxygenAwesomeCss)
-  set(DOXYGEN_GENERATE_TREEVIEW     YES)
-  set(DOXYGEN_HAVE_DOT              YES)
-  set(DOXYGEN_DOT_IMAGE_FORMAT      svg)
-  set(DOXYGEN_DOT_TRANSPARENT       YES)
-  set(DOXYGEN_HTML_EXTRA_STYLESHEET
-      ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome.css)
+    set(DOXYGEN_GENERATE_TREEVIEW     YES)
+    set(DOXYGEN_HAVE_DOT              YES)
+    set(DOXYGEN_DOT_IMAGE_FORMAT      svg)
+    set(DOXYGEN_DOT_TRANSPARENT       YES)
+    set(DOXYGEN_HTML_EXTRA_STYLESHEET
+        ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome.css)
 endmacro()
 
 macro(UseDoxygenAwesomeExtensions)
-  set(DOXYGEN_HTML_EXTRA_FILES
-    ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome-darkmode-toggle.js
-    ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome-fragment-copy-button.js
-    ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome-paragraph-link.js
-    ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome-interactive-toc.js
-  )
+    set(DOXYGEN_HTML_EXTRA_FILES
+        ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome-darkmode-toggle.js
+        ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome-fragment-copy-button.js
+        ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome-paragraph-link.js
+        ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome-interactive-toc.js
+    )
 
-  execute_process(COMMAND doxygen -w html header.html footer.html style.css
-                  WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
-  execute_process(COMMAND sed -i "/<\\/head>/r ${PROJECT_SOURCE_DIR}/cmake/extra_headers.html" header.html
-                  WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
-  set(DOXYGEN_HTML_HEADER ${PROJECT_BINARY_DIR}/header.html)
+    execute_process(COMMAND doxygen -w html header.html footer.html style.css
+                    WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
+    execute_process(COMMAND sed -i "/<\\/head>/r ${PROJECT_SOURCE_DIR}/cmake/extra_headers.html" header.html
+                    WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
+    set(DOXYGEN_HTML_HEADER ${PROJECT_BINARY_DIR}/header.html)
 endmacro()
