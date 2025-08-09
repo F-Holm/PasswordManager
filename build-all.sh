45e4d6497edaa1b354769e66f42ed74cf5c906ad
F-Holm <moshishafcdodgecharger@gmail.com>
2025-08-09 23:19:49 +0000

    clean ccache caché && disable wxWidgets

--- build-all.sh
+++ build-all.sh
@@ -2,21 +2,22 @@
 
 # Borrar /build y resetear openssl-src
 ./clean.sh
+ccache -C
 
 # Build workflows (los comentados todavía no están testeados pero ya están configurados)
-#cmake --workflow --preset wf-windows-x64-release
-#cmake --workflow --preset wf-windows-x64-debug
-#cmake --workflow --preset wf-windows-x86-release
-#cmake --workflow --preset wf-windows-x86-debug
-#cmake --workflow --preset wf-windows-arm64-release
-#cmake --workflow --preset wf-windows-arm64-debug
-#cmake --workflow --preset wf-windows-arm-release
-#cmake --workflow --preset wf-windows-arm-debug
-cmake --workflow --preset wf-linux-x64-release
-cmake --workflow --preset wf-linux-x64-debug
-#cmake --workflow --preset wf-linux-x86-release
-#cmake --workflow --preset wf-linux-x86-debug
-#cmake --workflow --preset wf-linux-arm64-release
-#cmake --workflow --preset wf-linux-arm64-debug
-#cmake --workflow --preset wf-linux-arm-release
-#cmake --workflow --preset wf-linux-arm-debug
+#cmake --workflow --preset wf-windows-x64-release && ccache -C
+#cmake --workflow --preset wf-windows-x64-debug && ccache -C
+#cmake --workflow --preset wf-windows-x86-release && ccache -C
+#cmake --workflow --preset wf-windows-x86-debug && ccache -C
+#cmake --workflow --preset wf-windows-arm64-release && ccache -C
+#cmake --workflow --preset wf-windows-arm64-debug && ccache -C
+#cmake --workflow --preset wf-windows-arm-release && ccache -C
+#cmake --workflow --preset wf-windows-arm-debug && ccache -C
+cmake --workflow --preset wf-linux-x64-release && ccache -C
+cmake --workflow --preset wf-linux-x64-debug && ccache -C
+#cmake --workflow --preset wf-linux-x86-release && ccache -C
+#cmake --workflow --preset wf-linux-x86-debug && ccache -C
+#cmake --workflow --preset wf-linux-arm64-release && ccache -C
+#cmake --workflow --preset wf-linux-arm64-debug && ccache -C
+#cmake --workflow --preset wf-linux-arm-release && ccache -C
+#cmake --workflow --preset wf-linux-arm-debug && ccache -C
