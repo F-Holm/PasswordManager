ff4af496a5fb2826b13a73701a1032aecf2c56cf
F-Holm <moshishafcdodgecharger@gmail.com>
2025-08-10 16:32:21 +0000

    add: check-platform to tests

--- workflow.sh
+++ workflow.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-./build-all.sh -l -x64 -d -c
+#./build-all.sh -l -x86 -d -g
+cmake --workflow --preset wf-linux-x64-debug-clang
+
+# No funciona:
+
+# Linux armv7 GCC       - (is not able to compile a simple test program)
+
+# Linux x86 Clang       - (is not able to compile a simple test program)
+# Linux arm64 Clang     - (is not able to compile a simple test program)
+# Linux armv7 Clang     - (is not able to compile a simple test program)
+
+# Windows x64 GCC       - (is not able to compile a simple test program)
+# Windows x86 GCC       - (is not able to compile a simple test program)
+# Windows arm64 GCC     - (is not able to compile a simple test program)
+# Windows armv7 GCC     - (is not able to compile a simple test program)
+
+# Windows x64 Clang     - (is not able to compile a simple test program)
+# Windows x86 Clang     - (is not able to compile a simple test program)
+# Windows arm64 Clang   - (is not able to compile a simple test program)
+# Windows armv7 Clang   - (is not able to compile a simple test program)
