d9008c736876e07dc09f8764fb37b2c5792e7047
F-Holm <moshishafcdodgecharger@gmail.com>
2025-08-13 13:29:08 -0300

    add: clang-tidy & desactivado (por ahora)

--- workflow.sh
+++ workflow.sh
@@ -4,8 +4,6 @@ cmake --workflow --preset wf-linux-x64-debug-clang
 
 # No funciona:
 
-# Linux armv7 GCC       - (is not able to compile a simple test program)
-
 # Linux x86 Clang       - (is not able to compile a simple test program)
 # Linux arm64 Clang     - (is not able to compile a simple test program)
 # Linux armv7 Clang     - (is not able to compile a simple test program)
