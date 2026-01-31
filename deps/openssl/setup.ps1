70a93788c4610f6303ff36a7b795463ad4fb34eb
Holm <you@example.com>
2026-01-31 19:03:53 +0000

    build(openssl): finish openssl setup script

--- deps/openssl/setup.ps1
+++ deps/openssl/setup.ps1
@@ -1,12 +1,14 @@
 #!/usr/bin/env pwsh
 
+$ErrorActionPreference = "Stop"
+
 $OpenSSLVersion = "3.6.1"
 $OpenSSLName    = "openssl-$OpenSSLVersion"
 $Url            = "https://github.com/openssl/openssl/releases/download/$OpenSSLName/$OpenSSLName.tar.gz"
 
 $SrcDir        = "$PSScriptRoot/$OpenSSLName"
-$Tarball       = Join-Path $SrcDir "$OpenSSLName.tar.gz"
-$InstallDir    = Join-Path $SrcDir "$PSScriptRoot/../../lib/openssl/"
+$Tarball       = Join-Path $PSScriptRoot "$OpenSSLName.tar.gz"
+$InstallDir    = Join-Path $PSScriptRoot "../../lib/openssl/"
 $LlvmMingwBinPath = "/opt/llvm-mingw/bin"
 
 Set-Location $PSScriptRoot
@@ -30,12 +32,11 @@ if (-not (Test-Path $Tarball)) {
 # -------------------------------------------------------------------
 
 if (Test-Path $SrcDir) {
-    Write-Host "==> Removing previous source directory"
-    Remove-Item -Recurse -Force $SrcDir
+    throw "Delete $SrcDir"
 }
 
 Write-Host "==> Extracting OpenSSL..."
-tar -xzf $Tarball -C $SrcDir
+tar -xzf $Tarball -C $PSScriptRoot
 
 Set-Location $SrcDir
 
@@ -46,33 +47,39 @@ Set-Location $SrcDir
 # SO | Arch | Compiler | Compiler Prefix | UCRT Flags | Target
 
 $configs = @(
-    @{ OS="linux";   Arch="x64";   CC="clang"; Prefix="";                                      UCRT=0; Target="linux-x86_64-clang" }
-    #@{ OS="linux";   Arch="x86";   CC="gcc";   Prefix="$LlvmMingwBinPath/i686-linux-gnu-";     UCRT=0; Target="linux-x86" },
-    #@{ OS="linux";   Arch="arm64"; CC="gcc";   Prefix="$LlvmMingwBinPath/aarch64-linux-gnu-";  UCRT=0; Target="linux-aarch64" },
-    #@{ OS="windows"; Arch="x64";   CC="clang"; Prefix="$LlvmMingwBinPath/x86_64-w64-mingw32-"; UCRT=1; Target="mingw64" },
-    #@{ OS="windows"; Arch="x86";   CC="clang"; Prefix="$LlvmMingwBinPath/i686-w64-mingw32-";   UCRT=1; Target="mingw" },
-    #@{ OS="windows"; Arch="arm64"; CC="clang"; Prefix="$LlvmMingwBinPath/aarch64-w64-mingw32-";UCRT=1; Target="mingwarm64" }
+    @{ OS="linux";   Arch="x64";   CC="clang"; Prefix="";                                      UCRT=0; Target="linux-x86_64-clang" },
+    @{ OS="linux";   Arch="x86";   CC="gcc";   Prefix="i686-linux-gnu-";                       UCRT=0; Target="linux-x86" },
+    @{ OS="linux";   Arch="arm64"; CC="gcc";   Prefix="aarch64-linux-gnu-";                    UCRT=0; Target="linux-aarch64" },
+    @{ OS="windows"; Arch="x64";   CC="clang"; Prefix="$LlvmMingwBinPath/x86_64-w64-mingw32-"; UCRT=1; Target="mingw64" }
+    @{ OS="windows"; Arch="x86";   CC="clang"; Prefix="$LlvmMingwBinPath/i686-w64-mingw32-";   UCRT=1; Target="mingw" },
+    @{ OS="windows"; Arch="arm64"; CC="clang"; Prefix="$LlvmMingwBinPath/aarch64-w64-mingw32-";UCRT=1; Target="mingwarm64" }
 )
 
 $Jobs      = [Environment]::ProcessorCount
-$BaseFlags = 'shared no-tests no-apps no-docs'
+$BaseFlags = 'no-shared'# no-tests no-apps no-docs no-zlib
 
 foreach ($cfg in $configs) {
     Write-Host "==> Building $($cfg.OS)-$($cfg.Arch)"
     
     $env:CC = $cfg.CC
-    if ($cfg.UCRT -eq 1) {
-        $env:CFLAGS   = "-D__USE_MINGW_ANSI_STDIO=1"
+    if ($cfg.UCRT -eq 1 -and $cfg.OS -eq "windows") {
+        $env:CFLAGS   = "-D__USE_MINGW_ANSI_STDIO=1 -DSIO_UDP_NETRESET=_WSAIOW\(IOC_VENDOR,12\)"
         $env:CXXFLAGS = $env:CFLAGS
     } else {
         $env:CFLAGS   = ""
         $env:CXXFLAGS = ""
     }
     
-    "perl Configure $CrossCompilerPrefix$matrix[$i,3] $matrix[$i,5] $BaseFlags --prefix=$InstallDir$matrix[$i,0]-$matrix[$i,1]"
-
     perl Configure --cross-compile-prefix=$($cfg.Prefix) $($cfg.Target) $BaseFlags --prefix="$InstallDir/$($cfg.OS)-$($cfg.Arch)"
-    make -j$Jobs
+    make "-j$Jobs"
     make install_sw
     make clean
 }
+
+# -------------------------------------------------------------------
+# Clean
+# -------------------------------------------------------------------
+
+Set-Location $PSScriptRoot
+Remove-Item -Recurse -Force $SrcDir
+Remove-Item -Force $Tarball
