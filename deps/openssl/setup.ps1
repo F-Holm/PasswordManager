73db9b836b4b7e1201abee49bdb51dad85f6e1ea
F-Holm <holmfedericoa@gmail.com>
2026-02-01 00:51:31 +0000

    build(wxwidgets): builds Linux x64 & Windows

--- deps/openssl/setup.ps1
+++ deps/openssl/setup.ps1
@@ -6,7 +6,7 @@ $OpenSSLVersion = "3.6.1"
 $OpenSSLName    = "openssl-$OpenSSLVersion"
 $Url            = "https://github.com/openssl/openssl/releases/download/$OpenSSLName/$OpenSSLName.tar.gz"
 
-$SrcDir        = "$PSScriptRoot/$OpenSSLName"
+$SrcDir        = Join-Path $PSScriptRoot $OpenSSLName
 $Tarball       = Join-Path $PSScriptRoot "$OpenSSLName.tar.gz"
 $InstallDir    = Join-Path $PSScriptRoot "../../lib/openssl/"
 $LlvmMingwBinPath = "/opt/llvm-mingw/bin"
@@ -14,7 +14,7 @@ $LlvmMingwBinPath = "/opt/llvm-mingw/bin"
 Set-Location $PSScriptRoot
 
 Write-Host "==> OpenSSL version: $OpenSSLVersion"
-Write-Host "==> Working dir: $SrcDir"
+Write-Host "==> Working dir: $PSScriptRoot"
 
 # -------------------------------------------------------------------
 # Download
@@ -39,6 +39,7 @@ Write-Host "==> Extracting OpenSSL..."
 tar -xzf $Tarball -C $PSScriptRoot
 
 Set-Location $SrcDir
+Write-Host "==> Working dir: $SrcDir"
 
 # -------------------------------------------------------------------
 # Configuration and Build
@@ -47,12 +48,12 @@ Set-Location $SrcDir
 # SO | Arch | Compiler | Compiler Prefix | UCRT Flags | Target
 
 $configs = @(
-    @{ OS="linux";   Arch="x64";   CC="clang"; Prefix="";                                      UCRT=0; Target="linux-x86_64-clang" },
-    @{ OS="linux";   Arch="x86";   CC="gcc";   Prefix="i686-linux-gnu-";                       UCRT=0; Target="linux-x86" },
-    @{ OS="linux";   Arch="arm64"; CC="gcc";   Prefix="aarch64-linux-gnu-";                    UCRT=0; Target="linux-aarch64" },
-    @{ OS="windows"; Arch="x64";   CC="clang"; Prefix="$LlvmMingwBinPath/x86_64-w64-mingw32-"; UCRT=1; Target="mingw64" }
-    @{ OS="windows"; Arch="x86";   CC="clang"; Prefix="$LlvmMingwBinPath/i686-w64-mingw32-";   UCRT=1; Target="mingw" },
-    @{ OS="windows"; Arch="arm64"; CC="clang"; Prefix="$LlvmMingwBinPath/aarch64-w64-mingw32-";UCRT=1; Target="mingwarm64" }
+    @{ OS="Linux";   Arch="x86_64";  CC="clang"; Prefix="";                                       UCRT=0; Target="linux-x86_64-clang" },
+    @{ OS="Linux";   Arch="x86";     CC="gcc";   Prefix="i686-linux-gnu-";                        UCRT=0; Target="linux-x86" },
+    @{ OS="Linux";   Arch="aarch64"; CC="gcc";   Prefix="aarch64-linux-gnu-";                     UCRT=0; Target="linux-aarch64" },
+    @{ OS="Windows"; Arch="x86_64";  CC="clang"; Prefix="$LlvmMingwBinPath/x86_64-w64-mingw32-";  UCRT=1; Target="mingw64" },
+    @{ OS="Windows"; Arch="x86";     CC="clang"; Prefix="$LlvmMingwBinPath/i686-w64-mingw32-";    UCRT=1; Target="mingw" },
+    @{ OS="Windows"; Arch="aarch64"; CC="clang"; Prefix="$LlvmMingwBinPath/aarch64-w64-mingw32-"; UCRT=1; Target="mingwarm64" }
 )
 
 $Jobs      = [Environment]::ProcessorCount
--- deps/wxwidgets/setup.ps1
+++ deps/wxwidgets/setup.ps1
@@ -1,3 +1,87 @@
 #!/usr/bin/env pwsh
 
-$ScriptDir = $PSScriptRoot
+$ErrorActionPreference = "Stop"
+
+$WxWidgetsVersion = "3.2.9"
+$WxWidgetsName    = "wxWidgets-$WxWidgetsVersion"
+$Url            = "https://github.com/wxWidgets/wxWidgets/releases/download/v$WxWidgetsVersion/$WxWidgetsName.tar.bz2"
+
+$SrcDir        = Join-Path $PSScriptRoot $WxWidgetsName
+$Tarball       = Join-Path $PSScriptRoot "$WxWidgetsName.tar.bz2"
+$InstallDir    = Join-Path $PSScriptRoot "../../lib/wxwidgets/"
+$LlvmMingwBinPath = "/opt/llvm-mingw/bin"
+
+Set-Location $PSScriptRoot
+
+Write-Host "==> wxWidgets version: $WxWidgetsVersion"
+Write-Host "==> Working dir: $PSScriptRoot"
+
+# -------------------------------------------------------------------
+# Download
+# -------------------------------------------------------------------
+
+if (-not (Test-Path $Tarball)) {
+    Write-Host "==> Downloading wxWidgets..."
+    Invoke-WebRequest -Uri $Url -OutFile $Tarball
+} else {
+    Write-Host "==> Tarball already exists, skipping download"
+}
+
+# -------------------------------------------------------------------
+# Extraction
+# -------------------------------------------------------------------
+
+if (Test-Path $SrcDir) {
+    throw "Delete $SrcDir"
+}
+
+Write-Host "==> Extracting wxWidgets..."
+tar -xjf $Tarball -C $PSScriptRoot
+
+Set-Location $SrcDir
+Write-Host "==> Working dir: $SrcDir"
+
+# -------------------------------------------------------------------
+# Configuration and Build
+# -------------------------------------------------------------------
+
+$configs = @(
+    @{ OS="Linux";   Arch="x86_64";  CC="clang"; CXXC="clang++"; Prefix="" },
+    #@{ OS="Linux";   Arch="x86";     CC="gcc";   CC="g++";       Prefix="i686-linux-gnu-" },
+    #@{ OS="Linux";   Arch="aarch64"; CC="gcc";   CC="g++";       Prefix="aarch64-linux-gnu-" },
+    @{ OS="Windows"; Arch="x86_64";  CC="clang"; CXXC="clang++"; Prefix="$LlvmMingwBinPath/x86_64-w64-mingw32-" },
+    @{ OS="Windows"; Arch="x86";     CC="clang"; CXXC="clang++"; Prefix="$LlvmMingwBinPath/i686-w64-mingw32-" },
+    @{ OS="Windows"; Arch="aarch64"; CC="clang"; CXXC="clang++"; Prefix="$LlvmMingwBinPath/aarch64-w64-mingw32-" }
+)
+
+foreach ($cfg in $configs) {
+    Write-Host "==> Building $($cfg.OS)-$($cfg.Arch)"
+
+    $BuildDir = Join-Path $InstallDir "$($cfg.OS)-$($cfg.Arch)"
+    New-Item -Path $BuildDir -ItemType Directory
+    Set-Location $BuildDir
+
+    $ExtraFlags = ""
+    if ($($cfg.OS) -eq "Linux" -and $($cfg.Arch) -ne "x86_64"){
+        $ExtraFlags = "$ExtraFlags -DCMAKE_SYSROOT=$HOME/cross-env/sysroot-arm64 -DCMAKE_FIND_ROOT_PATH=$HOME/cross-env/sysroot-arm64"
+    }
+
+    cmake $SrcDir -G Ninja `
+        "-DCMAKE_C_COMPILER=$($cfg.Prefix)$($cfg.CC)" `
+        "-DCMAKE_CXX_COMPILER=$($cfg.Prefix)$($cfg.CXXC)" `
+        "-DCMAKE_SYSTEM_NAME=$($cfg.OS)" `
+        "-DCMAKE_SYSTEM_PROCESSOR=$($cfg.Arch)" `
+        "-DwxBUILD_SHARED=OFF" `
+        "-DCMAKE_BUILD_TYPE=Release" `
+        $ExtraFlags
+
+    ninja
+}
+
+# -------------------------------------------------------------------
+# Clean
+# -------------------------------------------------------------------
+
+Set-Location $PSScriptRoot
+Remove-Item -Recurse -Force $SrcDir
+Remove-Item -Force $Tarball
