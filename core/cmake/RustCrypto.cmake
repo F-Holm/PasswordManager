4803501e9cbe6fcd7f8ef02f93b08402e2b83e0e
Holm <you@example.com>
2026-02-12 15:55:30 -0300

    fix(core/rust_crypto): add corrosion & fix windows library error

--- core/cmake/RustCrypto.cmake
+++ core/cmake/RustCrypto.cmake
@@ -1,45 +1,22 @@
+include(FetchContent)
+
+FetchContent_Declare(
+  corrosion
+  GIT_REPOSITORY https://github.com/corrosion-rs/corrosion.git
+  GIT_TAG v0.6.1
+)
+
+FetchContent_MakeAvailable(corrosion)
+
+corrosion_import_crate(
+    MANIFEST_PATH ${CMAKE_SOURCE_DIR}/../crypto/Cargo.toml
+)
+
 if (WIN32)
-    set(CRYPTO_LIB_NAME "rust_crypto.lib")
-else()
-    set(CRYPTO_LIB_NAME "librust_crypto.a")
+    target_link_libraries(rust_crypto INTERFACE bcrypt)
 endif()
-set(CRYPTO_HEADER_NAME "rust_crypto.h")
 
-set(CRYPTO_SRC_DIR "${CMAKE_SOURCE_DIR}/../crypto")
-set(CRYPTO_BINARY_DIR_ "${CRYPTO_SRC_DIR}/target/release")
-
-set(CRYPTO_BINARY_PATH "${CRYPTO_BINARY_DIR_}/${CRYPTO_LIB_NAME}")
-set(CRYPTO_HEADER_PATH "${CRYPTO_BINARY_DIR_}/${CRYPTO_HEADER_NAME}")
-
-set(RUST_NEW_DIR "${CMAKE_BINARY_DIR}/rust") 
-set(CRYPTO_NEW_DIR "${RUST_NEW_DIR}/rust_crypto")
-set(CRYPTO_BINARY_NEW_PATH "${CRYPTO_NEW_DIR}/${CRYPTO_LIB_NAME}")
-set(CRYPTO_HEADER_NEW_PATH "${CRYPTO_NEW_DIR}/${CRYPTO_HEADER_NAME}")
-
-file(MAKE_DIRECTORY ${CRYPTO_NEW_DIR})
-
-add_custom_command(
-    OUTPUT ${CRYPTO_BINARY_NEW_PATH}
-    COMMAND cargo build --release
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CRYPTO_NEW_DIR}
-    COMMAND ${CMAKE_COMMAND} -E copy
-            "${CRYPTO_BINARY_PATH}"
-            "${CRYPTO_BINARY_NEW_PATH}"
-    COMMAND ${CMAKE_COMMAND} -E copy
-            "${CRYPTO_HEADER_PATH}"
-            "${CRYPTO_HEADER_NEW_PATH}"
-    WORKING_DIRECTORY "${CRYPTO_SRC_DIR}"
-    COMMENT "Building Rust crypto library"
-)
-
-add_custom_target(rust_crypto_build ALL
-    DEPENDS ${CRYPTO_BINARY_NEW_PATH}
-)
-
-add_library(rust_crypto STATIC IMPORTED)
-add_dependencies(rust_crypto rust_crypto_build)
-
-set_target_properties(rust_crypto PROPERTIES
-    IMPORTED_LOCATION "${CRYPTO_BINARY_NEW_PATH}"
-    INTERFACE_INCLUDE_DIRECTORIES "${RUST_NEW_DIR}"
+target_include_directories(rust_crypto
+    INTERFACE
+        ${CMAKE_SOURCE_DIR}/../crypto/include
 )
