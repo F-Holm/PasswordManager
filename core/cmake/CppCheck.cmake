6f947fed1b811efbcde4ac84767b276849bb8f8e
Holm <you@example.com>
2026-02-12 16:01:32 -0300

    fix(core/cppcheck): fix cppcheck path on windows

--- core/cmake/CppCheck.cmake
+++ core/cmake/CppCheck.cmake
@@ -1,5 +1,15 @@
 function(AddCppCheck target)
-    find_program(CPPCHECK_PATH cppcheck)
+    if (WIN32)
+        find_program(CPPCHECK_PATH
+            NAMES cppcheck
+            PATHS
+                "C:/Program Files/Cppcheck"
+            NO_DEFAULT_PATH
+        )
+    else()
+        find_program(CPPCHECK_PATH cppcheck)
+    endif()
+
     if (CPPCHECK_PATH)
         set_target_properties("${target}"
             PROPERTIES CXX_CPPCHECK
