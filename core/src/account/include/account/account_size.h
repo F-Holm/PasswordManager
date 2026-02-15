30720bc44afe8ae8fea0c5b7c3ef1b175096dd27
F-Holm <holmfedericoa@gmail.com>
2026-02-15 16:14:38 +0000

    feat(core/core_interface): create core_interface header

--- core/src/account/include/account/account_size.h
+++ core/src/account/include/account/account_size.h
@@ -4,10 +4,9 @@
 
 namespace AccountSize {
 static constexpr std::size_t kDescription = 128;
-static constexpr std::size_t kEmail = 128;
+static constexpr std::size_t kEmail = 256;
 static constexpr std::size_t kUsername = 128;
-static constexpr std::size_t kPassword = 128;
+static constexpr std::size_t kPassword = 256;
 static constexpr std::size_t kExtra = 1024;
-static constexpr std::size_t kDataSize =
-    kEmail + kUsername + kPassword + kExtra;
+static constexpr std::size_t kData = kEmail + kUsername + kPassword + kExtra;
 }  // namespace AccountSize
