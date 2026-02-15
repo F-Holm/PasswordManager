2b0d2d1d0710e25fdbc03090dac2c05a5ca43ccf
Holm <you@example.com>
2026-02-15 16:14:38 +0000

    feat(core/core_interface): create core_interface header

--- core/src/account/include/account/account_binary.h
+++ core/src/account/include/account/account_binary.h
@@ -5,7 +5,7 @@
 
 struct AccountBinary {
   std::byte description[AccountSize::kDescription];
-  std::byte data[AccountSize::kDataSize];
+  std::byte data[AccountSize::kData];
   std::byte iv_description[Crypto::Size::kIv];
   std::byte iv_data[Crypto::Size::kIv];
   std::byte tag_description[Crypto::Size::kTag];
