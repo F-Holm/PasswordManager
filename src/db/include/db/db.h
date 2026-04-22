ec53306fde5b40cf9bf467f7bfddcced40188dbe
F-Holm <moshishafcdodgecharger@gmail.com>
2026-04-22 13:46:53 +0000

    feat(db): add function declarations to db.h

--- src/db/include/db/db.h
+++ src/db/include/db/db.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 #include "db/storage.h"
 #include "types/account_binary.h"
 #include "types/account_index.h"
@@ -8,6 +10,18 @@
 class DB {
  public:
   void Open();
+  void Close();
+
+  AccountIndex SwapIndex(std::uint32_t elem1, std::uint32_t elem2);
+  AccountIndex GetAllIndex();
+  void AddAccount(const AccountBinary& account);
+  AccountBinary GetAccount(std::uint32_t pos);
+  void UpdateAccount(std::uint32_t pos, const AccountBinary& account);
+  void DeleteAccount(std::uint32_t pos);
+  void Compact();
+
+  DB();
+  ~DB();
 
  private:
   Storage<AccountBinary> vault;
